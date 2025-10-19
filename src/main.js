8dc69318059e07e7f5f284f4c3e03329fab1a9b6
Michael Kintscher <michael@kintscher.dev>
2025-10-19 13:31:05 -0700

    Welcome page now displayed when browser is installed

---
+++ src/main.js
@@ -0,0 +1,20 @@
+//
+// ***********************************************************************************
+//                      ONBOARDING, UPBOARDING, AND OFFBOARDING
+//
+// ***********************************************************************************
+//
+
+browser.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
+  //if (temporary) return; // skip during development
+  switch (reason) {
+    case "install":
+      {
+        const url = browser.runtime.getURL("../docs/welcome.html");
+        await browser.tabs.create({ url });
+        // or: await browser.windows.create({ url, type: "popup", height: 600, width: 600, });
+      }
+      break;
+    // see below
+  }
+});
