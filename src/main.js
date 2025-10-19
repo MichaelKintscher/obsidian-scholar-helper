3010cd561b2e513f40b236863ecdff373b423718
Michael Kintscher <michael@kintscher.dev>
2025-10-19 15:39:06 -0700

    Added styling to the welcome page.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 //
 // ***********************************************************************************
-//                      ONBOARDING, UPBOARDING, AND OFFBOARDING
+//                      ONBOARDING AND UPBOARDING
 //
 // ***********************************************************************************
 //
@@ -9,12 +9,26 @@ browser.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
   //if (temporary) return; // skip during development
   switch (reason) {
     case "install":
-      {
-        const url = browser.runtime.getURL("../docs/welcome.html");
-        await browser.tabs.create({ url });
-        // or: await browser.windows.create({ url, type: "popup", height: 600, width: 600, });
-      }
-      break;
-    // see below
+        {
+            const url = browser.runtime.getURL("../docs/welcome.html");
+            await browser.tabs.create({ url });
+            // or: await browser.windows.create({ url, type: "popup", height: 600, width: 600, });
+            break;
+        }
+    case "update":
+        {
+            // Not currently supported.
+            break;
+        }
   }
 });
+
+//
+// ***********************************************************************************
+//                      OFFBOARDING
+//
+// ***********************************************************************************
+//
+
+// TODO - set up a page for offboarding.
+//browser.runtime.setUninstallURL("");
